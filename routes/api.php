0ac4d7f62705831fab7f115ac47d986130ce888a
Lardin Armas <andresarmas97@hotmail.com>
2021-02-09 10:48:55 -0500

    api laravel finish

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use  App\Http\Controllers\Api\v1\PostController as PostV1;
+use  App\Http\Controllers\Api\v2\PostController as PostV2;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\v1\PostController::class)
-    ->only(['index','show', 'destroy']);
+// v1
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index','show', 'destroy'])
+    ->middleware('auth:sanctum');
+
+//v2
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['index','show'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [
+    App\Http\Controllers\Api\LoginController::class,
+    'login'
+]);
